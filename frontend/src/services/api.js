b387f0965c5131391fe780f15531c52843f9708e
Hyago Carvalho <carvalhohyago6@gmail.com>
2026-03-16 19:45:07 -0300

    feat: apontar frontend para backend producao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://hgc-sistema.onrender.com/api'
 })
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('hgc_token')
